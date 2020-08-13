62cc7df46fba25bef10d357ad7fcdf1eddf78c60
Ocean Bhardwaj <63634357+oceanbhardwaj@users.noreply.github.com>
2020-08-13 16:58:59 +0530

    Add files via upload

---
+++ SumOfDigits.java
@@ -0,0 +1,17 @@
+class SumOfDigits
+{
+	public static void main(String[] args) {
+		{
+			int n=123;
+			int sum=0;
+			while(n>0)
+			{
+				int d=n%10;
+				sum=sum+d;
+				n=n/10;
+			}
+			System.out.println(sum);
+
+		}
+	}
+}
