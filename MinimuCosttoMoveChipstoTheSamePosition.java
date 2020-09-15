761a6a47cb64670b90d82faaae9d65a9600e06a8
Ocean Bhardwaj <63634357+oceanbhardwaj@users.noreply.github.com>
2020-09-15 22:34:00 +0530

    Add files via upload

---
+++ MinimuCosttoMoveChipstoTheSamePosition.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int minCostToMoveChips(int[] position) {
+        int n=position.length;
+        int even=0;
+        int odd=0;
+       for(int i=0;i<n;i++)
+       {
+           if(position[i]%2==0)
+               even++;
+           else
+               odd++;
+       }
+        return Math.min(odd,even);
+        
+    }
+}
