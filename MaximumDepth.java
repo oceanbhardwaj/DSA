922352edb013332b33e8db4fc59a59001926d643
Ocean Bhardwaj <63634357+oceanbhardwaj@users.noreply.github.com>
2020-10-27 02:00:50 +0530

    Add files via upload

---
+++ MaximumDepth.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int maxDepth(TreeNode root) {
+        if(root==null)
+            return 0;
+        if(root.left==null && root.right==null)
+            return 1;
+        if(root.left==null)
+            return 1+ maxDepth(root.right);
+        if(root.right==null)
+        return 1+maxDepth(root.left);
+        
+        return 1+Math.max(maxDepth(root.left),maxDepth(root.right));
+        
+        
+    }
+}
