058bb26f1ff5fffa9380ed8deba3eb4e569d273c
Ocean Bhardwaj <63634357+oceanbhardwaj@users.noreply.github.com>
2021-06-27 00:48:19 +0530

    Add files via upload

--- removeAllOccurances.java
+++ removeAllOccurances.java
@@ -16,3 +16,51 @@ class Solution {
         return s;
     }
 }
+/////////////////////////////////
+class Solution {
+    public String removeOccurrences(String s, String part) {
+        Stack<Character> stack=new Stack<>();
+        for(int i=0;i<s.length();i++)
+        {
+            stack.push(s.charAt(i));
+            if(stack.size()>=part.length())
+                compareString(stack,part);
+        }
+        StringBuilder sb=new StringBuilder();
+        while(!stack.isEmpty())
+        {
+            sb.append(stack.pop());
+        }
+        sb.reverse();
+        return sb.toString();
+        
+        
+    }
+    public void compareString(Stack<Character> stack,String part)
+    {
+        StringBuilder sb=new StringBuilder();
+        boolean flag=true;
+        int size=part.length();
+        while(size>0)
+        {
+            sb.append(stack.pop());
+            size--;
+        }
+        sb.reverse();
+        for(int i=0;i<part.length();i++) 
+        {
+            if(part.charAt(i)!=sb.charAt(i))
+            {
+                flag=false;
+                break;
+            }
+        }
+        if(!flag)
+        {
+            for(int i=0;i<sb.length();i++)
+            {
+                stack.push(sb.charAt(i));
+            }
+        }
+    }
+}
