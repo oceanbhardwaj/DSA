354b6a28580f8b2dced9c961fc177bd1156214ff
Ocean Bhardwaj <63634357+oceanbhardwaj@users.noreply.github.com>
2022-01-05 23:35:23 +0530

    Add files via upload

---
+++ Dfs.java
@@ -0,0 +1,61 @@
+// { Driver Code Starts
+// Initial Template for Java
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class GFG {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br =
+            new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine().trim());
+        while (T-- > 0) {
+            String[] s = br.readLine().trim().split(" ");
+            int V = Integer.parseInt(s[0]);
+            int E = Integer.parseInt(s[1]);
+            ArrayList<ArrayList<Integer>> adj =
+                new ArrayList<ArrayList<Integer>>();
+            for (int i = 0; i < V; i++) adj.add(new ArrayList<Integer>());
+            for (int i = 0; i < E; i++) {
+                String[] S = br.readLine().trim().split(" ");
+                int u = Integer.parseInt(S[0]);
+                int v = Integer.parseInt(S[1]);
+                adj.get(u).add(v);
+                adj.get(v).add(u);
+            }
+            Solution obj = new Solution();
+            ArrayList<Integer> ans = obj.dfsOfGraph(V, adj);
+            for (int i = 0; i < ans.size(); i++)
+                System.out.print(ans.get(i) + " ");
+            System.out.println();
+        }
+    }
+}
+// } Driver Code Ends
+
+
+class Solution {
+    // Function to return a list containing the DFS traversal of the graph.
+    public ArrayList<Integer> dfsOfGraph(int V, ArrayList<ArrayList<Integer>> adj) {
+      ArrayList<Integer> a=new ArrayList<>();
+      boolean visited[]=new boolean[V];
+      
+      for(int i=0;i<V;i++)
+      {
+          if(!visited[i])
+          dfs(a,adj,visited,i);
+      }
+      return a;
+    }
+    public void dfs(ArrayList<Integer> a,ArrayList<ArrayList<Integer>> adj,boolean visited[],int start){
+      if(visited[start])
+      return;
+      visited[start]=true;
+      a.add(start);
+      for(int i=0;i<adj.get(start).size();i++)
+      {
+          int curr=adj.get(start).get(i);
+          if(!visited[curr])
+          dfs(a,adj,visited,curr);
+      }
+    }
+}
