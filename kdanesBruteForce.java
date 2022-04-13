60e4c01becb7445328731354fb86154b6cbab524
Ocean Bhardwaj <63634357+oceanbhardwaj@users.noreply.github.com>
2022-04-13 16:41:33 +0530

    Add files via upload

---
+++ kdanesBruteForce.java
@@ -0,0 +1,53 @@
+// { Driver Code Starts
+import java.io.*;
+
+class Main {
+    
+	public static void main (String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int t = Integer.parseInt(br.readLine().trim()); //Inputting the testcases
+		while(t-->0){
+		    //size of array
+		    int n = Integer.parseInt(br.readLine().trim());
+		    int arr[] = new int[n];
+		    String inputLine[] = br.readLine().trim().split(" ");
+		    
+		    //adding elements
+		    for(int i=0; i<n; i++){
+		        arr[i] = Integer.parseInt(inputLine[i]);
+		    }
+		    
+		    Solution obj = new Solution();
+		    
+		    //calling maxSubarraySum() function
+		    System.out.println(obj.maxSubarraySum(arr, n));
+		}
+	}
+}
+
+// } Driver Code Ends
+
+
+class Solution{
+
+    // arr: input array
+    // n: size of array
+    //Function to find the sum of contiguous subarray with maximum sum.
+    long maxSubarraySum(int arr[], int n){
+    int max=Integer.MIN_VALUE;
+    for(int i=0;i<n;i++)
+    {
+        int sum=arr[i];
+        max=Math.max(arr[i],max);
+        for(int j=i+1;j<n;j++){
+            sum=sum+arr[j];
+            max=Math.max(sum,max);
+            
+        }
+    }
+       
+    return max;    
+    }
+    
+}
+
