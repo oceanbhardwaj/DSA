d5337c8bfbb51d026331761864442f62f28ae823
Ocean Bhardwaj <63634357+oceanbhardwaj@users.noreply.github.com>
2022-04-21 00:26:38 +0530

    Add files via upload

---
+++ countTotalSetBits.java
@@ -0,0 +1,49 @@
+// { Driver Code Starts
+//Initial Template for Java
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+import java.math.BigInteger;
+
+
+ // } Driver Code Ends
+//User function Template for Java
+
+class Solution{
+    
+    //Function to return sum of count of set bits in the integers from 1 to n.
+    public static int countSetBits(int n){
+    
+    int total=0;
+    for(int i=1;i<=n;i++){
+        int count=0;
+        int res=i;
+        while(res!=0){
+            res=res&(res-1);
+            count++;
+        }
+        total+=count;
+    }
+        return total;
+    }
+}
+
+// { Driver Code Starts.
+
+// Driver code
+class Main {
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		Scanner sc=new Scanner(System.in);
+		int t=sc.nextInt();//testcases
+		int x, n;
+		while(t-->0) {
+	        n = sc.nextInt();//input n
+
+		    Solution obj = new Solution();
+
+		    System.out.println(obj.countSetBits(n));//calling countSetBits() method
+		}
+	}
+}
+  // } Driver Code Ends
