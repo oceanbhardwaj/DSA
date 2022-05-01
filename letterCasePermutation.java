8a985fd08313c91fc3a1b8fc08872c9ec7c9ed44
Ocean Bhardwaj <63634357+oceanbhardwaj@users.noreply.github.com>
2022-05-01 17:50:45 +0530

    Add files via upload

--- letterCasePermutation.java
+++ letterCasePermutation.java
@@ -1,28 +1,28 @@
 class Solution {
     public List<String> letterCasePermutation(String s) {
-        List<String> res=new ArrayList<>();
-         call(s,0,res,"");
+         List<String> res=new ArrayList();
+        gettingLetterCasePermutation(s,res,0,"");
         return res;
     }
-    public void call(String s,int start,List<String> res,String ans)
-    {
-        if(start>=s.length())
+    public void gettingLetterCasePermutation(String s, List<String> res,int i,String output){
+        if(i>=s.length())
         {
-            res.add(ans);
+            res.add(output);
             return;
         }
-        String cap="";
-        String nonCap="";
-        if(Character.isAlphabetic(s.charAt(start)))
-        {
-            cap=ans+(s.charAt(start)+"").toUpperCase();
-            nonCap=ans+(""+s.charAt(start)).toLowerCase();
-            call(s,start+1,res,cap);
-            call(s,start+1,res,nonCap);
-        }
-        else
-        {
-            call(s,start+1,res,ans+s.charAt(start));
-        }
+          String str=s.charAt(i)+"";
+        str=str.toLowerCase();
+        
+        if(!Character.isDigit(s.charAt(i))){
+           gettingLetterCasePermutation(s,res,i+1,output+str) ;
+            String str1=s.charAt(i)+"";
+            str1=str1.toUpperCase();
+          
+           gettingLetterCasePermutation(s,res,i+1,output+str1) ;
+
+      }
+       else
+       gettingLetterCasePermutation(s,res,i+1,output+s.charAt(i)) ;
+
     }
 }
