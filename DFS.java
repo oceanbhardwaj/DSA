00277a11e9df9f57fde5cd4761f6f621890c90ab
Ocean Bhardwaj <63634357+oceanbhardwaj@users.noreply.github.com>
2022-05-03 23:32:01 +0530

    Add files via upload

--- DFS.java
+++ DFS.java
@@ -1,28 +1,57 @@
-class Solution
-{
-    //Function to return a list containing the DFS traversal of the graph.
-    public ArrayList<Integer> dfsOfGraph(int V, ArrayList<ArrayList<Integer>> adj)
-    {
-        ArrayList<Integer> a=new ArrayList<>();
-        boolean visited[]=new boolean[V];
-        dfs(a,adj,0,visited);
-        return a;
+// { Driver Code Starts
+// Initial Template for Java
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class GFG {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br =
+            new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine().trim());
+        while (T-- > 0) {
+            String[] s = br.readLine().trim().split(" ");
+            int V = Integer.parseInt(s[0]);
+            int E = Integer.parseInt(s[1]);
+            ArrayList<ArrayList<Integer>> adj =
+                new ArrayList<ArrayList<Integer>>();
+            for (int i = 0; i < V; i++) adj.add(new ArrayList<Integer>());
+            for (int i = 0; i < E; i++) {
+                String[] S = br.readLine().trim().split(" ");
+                int u = Integer.parseInt(S[0]);
+                int v = Integer.parseInt(S[1]);
+                adj.get(u).add(v);
+                adj.get(v).add(u);
+            }
+            Solution obj = new Solution();
+            ArrayList<Integer> ans = obj.dfsOfGraph(V, adj);
+            for (int i = 0; i < ans.size(); i++)
+                System.out.print(ans.get(i) + " ");
+            System.out.println();
+        }
     }
-    public void dfs(ArrayList<Integer> a,ArrayList<ArrayList<Integer>> adj,int start,boolean visited[])
-    {
+}
+// } Driver Code Ends
+
+
+class Solution {
+    // Function to return a list containing the DFS traversal of the graph.
+    public ArrayList<Integer> dfsOfGraph(int V, ArrayList<ArrayList<Integer>> adj) {
+       boolean visited[]=new boolean[V];
+       ArrayList<Integer> res=new ArrayList<>();
+       dfs(adj,visited,0,res);
+       return res;
+    }
+    public void dfs(ArrayList<ArrayList<Integer>> adj,boolean visited[],int i,ArrayList<Integer> res){
+        if(visited[i])
+        return;
         
-        if(visited[start])
-        return ;
-        visited[start]=true;
-        a.add(start);
-        for(int i=0;i<adj.get(start).size();i++)
-        {
-            int curr=adj.get(start).get(i);
-            if(!visited[curr])
-            {
-                dfs(a,adj,curr,visited);
+        visited[i]=true;
+        res.add(i);
+        for(int j=0;j<adj.get(i).size();j++){
+            int ele=adj.get(i).get(j);
+            if(!visited[ele]){
+                dfs(adj,visited,ele,res);
             }
         }
     }
-    
 }
