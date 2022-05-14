80c39977f99408a7ef3cfa7bcb7c691b84be30c8
Ocean Bhardwaj <63634357+oceanbhardwaj@users.noreply.github.com>
2022-05-14 09:36:57 +0530

    Add files via upload

---
+++ findAllPairs.java
@@ -0,0 +1,110 @@
+// { Driver Code Starts
+//Initial Template for Java
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+
+class pair  {  
+    long first, second;  
+    public pair(long first, long second)  
+    {  
+        this.first = first;  
+        this.second = second;  
+    }  
+}
+
+class GFG {
+	public static void main(String[] args) throws IOException
+	{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine().trim());
+        while(t-->0)
+        {
+            StringTokenizer stt = new StringTokenizer(br.readLine());
+            
+            long N = Long.parseLong(stt.nextToken());
+            long M = Long.parseLong(stt.nextToken());
+            long X = Long.parseLong(stt.nextToken());
+            long A[] = new long[(int)(N)];
+            long B[] = new long[(int)(M)];
+            
+            
+            String inputLine[] = br.readLine().trim().split(" ");
+            for (int i = 0; i < N; i++) {
+                A[i] = Long.parseLong(inputLine[i]);
+            }
+            String inputLine1[] = br.readLine().trim().split(" ");
+            for (int i = 0; i < M; i++) {
+                B[i] = Long.parseLong(inputLine1[i]);
+            }
+            
+            
+            Solution obj = new Solution();
+            pair [] answer = obj.allPairs(A, B, N, M, X);
+            int sz = answer.length;
+            
+            if(sz==0)
+            System.out.println(-1);
+            else{
+                StringBuilder output = new StringBuilder();
+                for(int i=0;i<sz;i++){
+                    if(i<sz-1)
+                    output.append(answer[i].first +" "+ answer[i].second + ", ");
+                    else
+                    output.append(answer[i].first +" "+ answer[i].second);
+                    
+                }
+                System.out.println(output);
+            }
+            
+        }
+	}
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+/*
+class pair  {  
+    long first, second;  
+    public pair(long first, long second)  
+    {  
+        this.first = first;  
+        this.second = second;  
+    }  
+}
+*/
+
+class Solution {
+    public pair[] allPairs( long A[], long B[], long N, long M, long X) {
+        
+    ArrayList<pair> p=new ArrayList<pair>();
+        //Arrays.sort(A);
+       // Arrays.sort(B);
+        for(int i=0;i<N;i++){
+            for(int j=0;j<M;j++){
+                if(A[i]+B[j]==X)
+                {
+                    p.add(new pair(A[i],B[j]));
+                }
+            }
+        }
+        pair p1[]=new pair[p.size()];
+        int count=0;
+        for(pair pp:p){
+            p1[count++]=new pair(pp.first,pp.second);
+           
+        }
+        //sort
+        Comparator co =new Comparator<pair>(){
+            public int compare(pair p1,pair p2){
+                return (int)(p1.first-p2.first);
+            }
+        };
+        Arrays.sort(p1,co);
+        return p1;
+    }
+}
