74cc4a5dce6303ab4be1bb46ec3584ac98c2570a
Ocean Bhardwaj <63634357+oceanbhardwaj@users.noreply.github.com>
2022-05-15 23:22:07 +0530

    Add files via upload

---
+++ findAllFourNumbers.java
@@ -0,0 +1,72 @@
+// { Driver Code Starts
+// Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br =
+            new BufferedReader(new InputStreamReader(System.in));
+        int tc = Integer.parseInt(br.readLine().trim());
+
+        while (tc-- > 0) {
+            String[] nk = br.readLine().trim().split(" ");
+            int n = Integer.parseInt(nk[0]);
+            int k = Integer.parseInt(nk[1]);
+
+            String[] str = br.readLine().trim().split(" ");
+            int[] a = new int[n];
+            for (int i = 0; i < n; i++) {
+                a[i] = Integer.parseInt(str[i]);
+            }
+
+            Solution sln = new Solution();
+            ArrayList<ArrayList<Integer>> ans = sln.fourSum(a, k);
+            for (ArrayList<Integer> v : ans) {
+                for (int u : v) {
+                    System.out.print(u + " ");
+                }
+                System.out.print("$");
+            }
+            if (ans.isEmpty()) {
+                System.out.print(-1);
+            }
+            System.out.println();
+        }
+    }
+}// } Driver Code Ends
+
+
+// User function Template for Java
+
+// arr[] : int input array of integers
+// k : the quadruple sum required
+
+class Solution {
+    public ArrayList<ArrayList<Integer>> fourSum(int[] arr, int k1) {
+       ArrayList<ArrayList<Integer>> res=new ArrayList<>();
+       int n=arr.length;
+       for(int i=0;i<n;i++){
+           for(int j=i+1;j<n;j++){
+               for(int k=j+1;k<n;k++){
+                   for(int l=k+1;l<n;l++){
+                       if(arr[i]+arr[j]+arr[k]+arr[l]==k1)
+                       {
+                           ArrayList<Integer> a=new ArrayList<>();
+                           a.add(arr[i]);
+                           a.add(arr[j]);
+                           a.add(arr[k]);
+                           a.add(arr[l]);
+                           Collections.sort(a);
+                           if(!res.contains(a))
+                           res.add(a);
+                           
+                       }
+                   }
+               }
+           }
+       }
+       return res;
+    }
+}
