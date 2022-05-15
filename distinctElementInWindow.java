9da5784cf6968bc00cbebc51795b2a9845096f08
Ocean Bhardwaj <63634357+oceanbhardwaj@users.noreply.github.com>
2022-05-15 23:46:20 +0530

    Add files via upload

---
+++ distinctElementInWindow.java
@@ -0,0 +1,47 @@
+// { Driver Code Starts
+import java.util.*;
+import java.io.*;
+import java.util.HashMap;
+
+class GFG
+{
+    public static void main(String args[])
+    {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while (t > 0)
+        {
+            int n = sc.nextInt();
+            int k = sc.nextInt();
+            int a[] = new int[n];
+            for (int i = 0; i < n; i++) 
+                a[i] = sc.nextInt();
+            Solution g = new Solution();
+            
+            ArrayList<Integer> ans = g.countDistinct(a, n, k);
+
+            for (Integer val: ans) 
+                System.out.print(val+" "); 
+            System.out.println();
+            t--;
+        }
+    }
+}// } Driver Code Ends
+
+
+class Solution
+{
+    ArrayList<Integer> countDistinct(int A[], int n, int k)
+    {
+       ArrayList<Integer> a=new ArrayList<>();
+       for(int i=0;i<=n-k;i++){
+           HashSet<Integer> h=new HashSet<>();
+           for(int j=i;j<(i+k);j++){
+               h.add(A[j]);
+           }
+           a.add(h.size());
+       }
+       return a;
+    }
+}
+
