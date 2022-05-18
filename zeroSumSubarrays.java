1f0345bc5e1721df23f76991aaf1b921bda40c0f
Ocean Bhardwaj <63634357+oceanbhardwaj@users.noreply.github.com>
2022-05-18 09:24:33 +0530

    Add files via upload

---
+++ zeroSumSubarrays.java
@@ -0,0 +1,56 @@
+// { Driver Code Starts
+//Initial Template for Java
+
+/*package whatever //do not write package name here */
+
+import java.io.*;
+import java.util.*;
+class GFG {
+	public static void main (String[] args) {
+		Scanner sc=new Scanner(System.in);
+		int t=sc.nextInt();
+		sc.nextLine();
+		while(t-->0)
+		{
+		    int n;
+		    n=sc.nextInt();
+		    
+		    long arr[]=new long[n];
+	
+		    
+		    for(int i=0;i<n;i++)
+		    {
+		        arr[i]=sc.nextLong();
+		    }
+            Solution ob = new Solution();
+		    System.out.println(ob.findSubarray(arr,n));
+		}
+		
+	}
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution{
+    //Function to count subarrays with sum equal to 0.
+    public static long findSubarray(long[] arr ,int n) 
+    {
+     HashMap<Long,Long> h=new HashMap<>();
+     long sum=0;
+     int count=0;
+     h.put((long)0,(long)1);
+     for(int i=0;i<n;i++){
+         sum+=arr[i];
+         if(h.containsKey(sum))
+         {
+             count+=h.get(sum);
+         }
+         h.put(sum,h.getOrDefault(sum,(long)0)+1);
+     }
+     return count;
+   
+     }
+    
+}
