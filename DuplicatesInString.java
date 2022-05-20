88d2771fa2a2dc21bc40f08f68f9a65ef08eb9f3
Ocean Bhardwaj <63634357+oceanbhardwaj@users.noreply.github.com>
2022-05-21 00:58:22 +0530

    Add files via upload

---
+++ DuplicatesInString.java
@@ -0,0 +1,37 @@
+class Solution {
+    String removeDups(String S) { //o(n)
+            StringBuilder sb=new StringBuilder();
+            sb.append(S.charAt(0));
+            for(int i=1;i<S.length();i++){
+               boolean flag=false;
+               for(int j=0;j<i;j++){
+                   if(S.charAt(i)==S.charAt(j)){
+                       flag=true;
+                       break;
+                   }
+               }
+               if(!flag){
+                   sb.append(S.charAt(i));
+               }
+            }
+            return sb.toString();
+    }
+}
+
+
+
+class Solution {
+    String removeDups(String S) { //o(n)
+        HashSet<Character> h=new HashSet<>();
+        StringBuilder sb=new StringBuilder();
+        for(int i=0;i<S.length();i++){
+            if(!h.contains(S.charAt(i))){
+                sb.append(S.charAt(i));
+                h.add(S.charAt(i));
+            }
+            
+            
+        }
+        return sb.toString();
+    }
+}
