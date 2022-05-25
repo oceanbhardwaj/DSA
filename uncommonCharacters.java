c504cf2406bdad9333c829e2c779a6f19ee53dda
Ocean Bhardwaj <63634357+oceanbhardwaj@users.noreply.github.com>
2022-05-25 18:54:39 +0530

    Add files via upload

---
+++ uncommonCharacters.java
@@ -0,0 +1,46 @@
+// { Driver Code Starts
+//Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+class GfG
+{
+    public static void main(String args[])throws IOException
+        {
+            Scanner sc = new Scanner(System.in);
+            int t = sc.nextInt();
+            while(t-->0)
+                {
+                    String A = sc.next();
+                    String B = sc.next();
+                    Solution ob = new Solution();
+                    System.out.println(ob.UncommonChars(A, B));
+                }
+        }
+}// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution
+{
+    String UncommonChars(String A, String B)
+    {
+       int count1[]=new int[26];
+       int count2[]=new int[26];
+       for(int i=0;i<A.length();i++){
+           count1[A.charAt(i)-'a']++;
+       }
+       for(int i=0;i<B.length();i++){
+           count2[B.charAt(i)-'a']++;
+       }
+       StringBuffer sb=new StringBuffer();
+       for(int i=0;i<26;i++){
+           if((count1[i]!=0 && count2[i]==0) ||( count1[i]==0 && count2[i]!=0))
+           sb.append((char)(i+97));
+       }
+       if(sb.toString().length()==0)
+       sb.append("-1");
+       return sb.toString();
+    }
+}
