084f69fb186e9b42e3d4130b14175f9a9ac25e26
Ocean Bhardwaj <63634357+oceanbhardwaj@users.noreply.github.com>
2022-05-29 18:08:45 +0530

    Add files via upload

---
+++ deleteMiddleBruteForce.java
@@ -0,0 +1,59 @@
+// { Driver Code Starts
+//Initial template for Java
+
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    public static void main (String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int t=sc.nextInt();
+        
+        while(t-->0)
+        {
+            int sizeOfStack =sc.nextInt();
+            Stack <Integer> myStack=new Stack<>();
+            
+            //adding elements to the stack
+            for(int i=0;i<sizeOfStack;i++)
+            {
+                int x=sc.nextInt();
+                myStack.push(x);
+                
+            }
+                Solution obj=new Solution();
+                obj.deleteMid(myStack,sizeOfStack);
+                
+                while(!myStack.isEmpty())
+                {
+                    System.out.print(myStack.peek()+" ");
+                    myStack.pop();
+                }
+                System.out.println();
+        }
+        
+        
+    }
+}// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution
+{
+    //Function to delete middle element of a stack.
+    public void deleteMid(Stack<Integer>s,int sizeOfStack){
+        Stack<Integer> stack=new Stack<>();
+        int c=sizeOfStack/2;
+        while(c-1>=0){
+            stack.push(s.pop());
+            c--;
+        }
+        s.pop();
+        while(!stack.isEmpty()){
+            s.push(stack.pop());
+        }
+        
+    } 
+}
+
