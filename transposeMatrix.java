b699d9c44062d10b00f1c822d271c22f62e292ee
Ocean Bhardwaj <63634357+oceanbhardwaj@users.noreply.github.com>
2022-06-02 16:30:44 +0530

    Add files via upload

---
+++ transposeMatrix.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int[][] transpose(int[][] matrix) {
+        int n=matrix.length;
+        int m=matrix[0].length;
+        int arr[][]=new int[m][n];
+        int row=0;
+        int col=0;
+        for(int i=0;i<n;i++){
+            row=0;
+            for(int j=0;j<m;j++){
+                arr[row++][col]=matrix[i][j];
+            }
+            col++;
+        }
+        return arr;
+    }
+}
