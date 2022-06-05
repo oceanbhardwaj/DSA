1b2649bf8b1f3a3ccecbc616a74a2e3ac17ec414
Ocean Bhardwaj <63634357+oceanbhardwaj@users.noreply.github.com>
2022-06-05 12:16:09 +0530

    Add files via upload

---
+++ doubleLinkedlistInsertion.java
@@ -0,0 +1,136 @@
+// { Driver Code Starts
+import java.util.*;
+
+class Node
+{
+	int data;
+	Node next;
+	Node prev;
+	Node(int data)
+	{
+	    this.data = data;
+	    next = prev = null;
+	}
+}
+
+class DLinkedList
+{
+	
+	Node newNode(Node head, int data)
+	{
+		Node n = new Node(data);
+		if(head == null)
+		{
+		    head = n;
+		    return head;
+		}
+		head.next = n;
+		n.prev = head;
+		head = n;
+		return head;
+	}
+	
+	
+	void printList(Node node)
+	{
+		Node temp = node;
+		while(temp.next != null)
+		{
+			temp = temp.next;
+		}
+		
+		while(temp.prev != null)
+		{
+			temp = temp.prev;
+		}
+		
+		while(temp != null)
+		{
+			System.out.print(temp.data+" ");
+			temp = temp.next;
+		}
+		System.out.println();
+	}
+	
+	public static void main(String args[])
+	{
+		Scanner sc = new Scanner(System.in);
+		DLinkedList DLL = new DLinkedList();
+		int t = sc.nextInt();
+		
+		while(t>0)
+		{
+			Node temp;
+			Node head = null;
+			Node root = null;
+			int n = sc.nextInt();
+			for(int i=0;i<n;i++)
+			{
+				int x = sc.nextInt();
+				head = DLL.newNode(head,x);
+			    if(root == null)    root = head;
+			}
+			head = root;
+			int pos = sc.nextInt();
+			int data = sc.nextInt();
+			
+		    GfG g = new GfG();
+		    g.addNode(head,pos,data);
+			
+			DLL.printList(head);
+			while(head.next != null)
+			{
+				temp = head;
+				head = head.next;
+			}
+		    t--;
+		}
+	}
+}// } Driver Code Ends
+
+
+/* Structure of Doubly Linked List
+class Node
+{
+	int data;
+	Node next;
+	Node prev;
+	Node(int data)
+	{
+	    this.data = data;
+	    next = prev = null;
+	}
+}*/
+
+class GfG
+{
+    //Function to insert a new node at given position in doubly linked list.
+    void addNode(Node head_ref, int pos, int data)
+	{
+	Node temp=new Node(data);
+	int count=0;
+	Node pre=null;
+		Node n=null;
+	if(head_ref.next!=null)
+	n=head_ref.next;
+
+	
+	Node dummy=head_ref;
+	if(head_ref==null)
+head_ref=temp;
+	while(count<pos && head_ref!=null){
+	    pre=head_ref;
+	    head_ref=head_ref.next;
+	    n=head_ref.next;
+	   // head_ref=head_ref.next;
+	    // n=head_ref.next;
+	    count++;
+	}
+	head_ref.next=temp;
+	temp.prev=head_ref;
+	temp.next=n;
+	if(n!=null)
+	n.prev=temp;
+//	return dummy;
+	}
+}
