cd54d8a90271b341999971675a3c25240008ef80
Ocean Bhardwaj <63634357+oceanbhardwaj@users.noreply.github.com>
2022-06-08 23:52:33 +0530

    Add files via upload

---
+++ removepalinDromicSubsequence.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int removePalindromeSub(String s) {
+        if(palindrome(s))
+            return 1;
+        return 2;
+        
+    }
+    public boolean palindrome(String s){
+        int i=0;
+        int j=s.length()-1;
+        while(i<j){
+            if(s.charAt(i)!=s.charAt(j))
+            {
+                return false;
+            }
+            i++;
+            j--;
+        }
+        return true;
+    }
+}
