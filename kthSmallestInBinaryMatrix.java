0e2648d4817aa5d72d7440d0a89ea26530251584
Ocean Bhardwaj <63634357+oceanbhardwaj@users.noreply.github.com>
2022-06-19 00:15:20 +0530

    Add files via upload

---
+++ kthSmallestInBinaryMatrix.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int kthSmallest(int[][] matrix, int k) {
+        PriorityQueue<Integer> pq=new PriorityQueue<Integer>(Collections.reverseOrder());
+        for(int i=0;i<matrix.length;i++){
+for(int j=0;j<matrix[0].length;j++){
+    pq.add(matrix[i][j]);
+          if(pq.size()>k){
+              pq.poll();
+          }
+}}
+        return pq.peek();
+    }
+}
