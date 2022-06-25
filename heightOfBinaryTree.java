ca13830370ad19e566caf4b038bcbe863a37dd3c
Ocean Bhardwaj <63634357+oceanbhardwaj@users.noreply.github.com>
2022-06-25 13:44:17 +0530

    Add files via upload

--- heightOfBinaryTree.java
+++ heightOfBinaryTree.java
@@ -20,3 +20,40 @@ class Solution {
         return 1+Math.max(maxDepth(root.left),maxDepth(root.right));
     }
 }
+---------------------------------------------------------------------------------------------
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public int maxDepth(TreeNode root) {
+        if(root==null)
+            return 0;
+        Queue<TreeNode> q=new LinkedList<>();
+        q.add(root);
+        int count=0;
+        while(!q.isEmpty()){
+            int size=q.size();
+            count++;
+            for(int i=0;i<size;i++){
+               TreeNode popped=q.poll();
+                if(popped.left!=null)
+                    q.add(popped.left);
+                if(popped.right!=null)
+                    q.add(popped.right);
+            }
+        }
+        return count;
+    }
+}
