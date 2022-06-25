8a9fa5bd5a6ea5f0ad1ebf1c07cdf0da2e54938c
Ocean Bhardwaj <63634357+oceanbhardwaj@users.noreply.github.com>
2022-06-25 13:29:23 +0530

    Add files via upload

--- InorderTreeTraversal.java
+++ InorderTreeTraversal.java
@@ -1,38 +1,54 @@
 
  Method -1 : Recursive  timeC=o(n)
- 
+
 class Solution {
-    public List<Integer> preorderTraversal(TreeNode root) {
-        List<Integer> a=new ArrayList<Integer>();
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> a=new ArrayList<>();
         if(root==null)
             return a;
-        PreOrder(root,a);
+        inOrder(root,a);
         return a;
     }
-    public void PreOrder(TreeNode root,List<Integer> a){
+    public void inOrder(TreeNode root,List<Integer> a){
         if(root==null)
             return;
+        inOrder(root.left,a);
         a.add(root.val);
-        PreOrder(root.left,a);
-        PreOrder(root.right,a);
+        inOrder(root.right,a);
+        
     }
 }
 ----------------------------------------------------
 Method 2 :iterative o(N)  o(N) space
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
 class Solution {
-    public List<Integer> preorderTraversal(TreeNode root) {
+    public List<Integer> inorderTraversal(TreeNode root) {
         List<Integer> a=new ArrayList<>();
         if(root==null)
             return a;
         Stack<TreeNode> s=new Stack<>();
         while(!s.isEmpty() || root!=null){
             while(root!=null){
-              s.push(root);
-                a.add(root.val);
+                s.push(root);
                 root=root.left;
             }
-            TreeNode poppedEle=s.pop();
-            root=poppedEle.right;
+            TreeNode popped=s.pop();
+            a.add(popped.val);
+            root=popped.right;
         }
         return a;
     }
