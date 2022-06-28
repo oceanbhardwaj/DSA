263563067ba5b804ad3b3f9a1e22cadf4ffec11e
Ocean Bhardwaj <63634357+oceanbhardwaj@users.noreply.github.com>
2022-06-28 08:46:51 +0530

    Add files via upload

--- zigzagLevelOrderTraversal.java
+++ zigzagLevelOrderTraversal.java
@@ -45,3 +45,59 @@ class Solution {
         return res;
     }
 }
+-----------------------------------------------------------
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
+        //TWO STACKS
+          List<List<Integer>> res=new ArrayList<>();
+        Stack<TreeNode> s1=new Stack<>();
+        Stack<TreeNode> s2=new Stack<>();
+        s1.push(root);
+        if(root==null)
+            return res;
+        while(!s1.isEmpty() || !s2.isEmpty()){
+            ArrayList<Integer> a=new ArrayList<>();
+            while(!s1.isEmpty()){
+                TreeNode popped=s1.pop();
+                a.add(popped.val);
+                 if(popped.left!=null)
+                    s2.push(popped.left);
+                if(popped.right!=null)
+                    s2.push(popped.right);
+               
+            }
+             if(!a.isEmpty())
+            res.add(a);
+            a=new ArrayList<>();
+            while(!s2.isEmpty()){
+                  TreeNode popped=s2.pop();
+                a.add(popped.val);
+               if(popped.right!=null)
+                    s1.push(popped.right);
+                if(popped.left!=null)
+                    s1.push(popped.left); 
+                 
+            }
+            if(!a.isEmpty())
+            res.add(a);
+          }
+        return res;
+       
+        
+    }
+}
