93390143cf2d5dfd5afa2543ef8fba745e76e59d
Ocean Bhardwaj <63634357+oceanbhardwaj@users.noreply.github.com>
2022-06-30 14:02:17 +0530

    Add files via upload

--- balancedTree.java
+++ balancedTree.java
@@ -27,3 +27,41 @@ class Solution {
         return 1+Math.max(Height(root.left),Height(root.right));
     }
 }
+--------------------------------------------
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean isBalanced(TreeNode root) {
+        if(root==null)
+            return true;
+        return call(root)!=-1;
+    }
+    public int call(TreeNode root){
+        if(root==null)
+            return 0;
+        int lh=call(root.left);
+        if(lh==-1)
+            return -1;
+        int rh=call(root.right);
+            if(rh==-1)
+                return -1;
+        
+        if(Math.abs(lh-rh)>1)
+            return -1;
+        else 
+            return Math.max(lh,rh)+1; 
+    }
+}
