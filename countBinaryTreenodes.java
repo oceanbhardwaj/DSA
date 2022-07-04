d05735639fae2e7bb9162eb03b719d389b750f82
Ocean Bhardwaj <63634357+oceanbhardwaj@users.noreply.github.com>
2022-07-04 23:34:44 +0530

    Add files via upload

---
+++ countBinaryTreenodes.java
@@ -0,0 +1,29 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    int count=0;
+    public int countNodes(TreeNode root) {
+         call(root);
+        return count;
+    }
+    public void call(TreeNode root){
+        if(root==null)
+            return;
+        count++;
+        call(root.left);
+        call(root.right);
+    }
+}
