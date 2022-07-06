2c73207da4160c09b0cf2b630652a9c5cb053cb2
Ocean Bhardwaj <63634357+oceanbhardwaj@users.noreply.github.com>
2022-07-06 10:59:29 +0530

    Add files via upload

---
+++ maxPathSum.java
@@ -0,0 +1,30 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    int max=Integer.MIN_VALUE;
+    public int maxPathSum(TreeNode root) {
+        maxPath(root);
+        return max;
+    }
+    public int maxPath(TreeNode root){
+        if(root==null)
+            return 0;
+        int lh=Math.max(0,maxPath(root.left));
+        int rh=Math.max(0,maxPath(root.right));
+        max=Math.max(max,lh+rh+root.val);
+        return root.val+Math.max(lh,rh);
+    }
+}
