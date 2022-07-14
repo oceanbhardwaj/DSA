f67ab7d2c076be999bb56646f251c1adb237007f
Ocean Bhardwaj <63634357+oceanbhardwaj@users.noreply.github.com>
2022-07-14 16:58:16 +0530

    Add files via upload

---
+++ lis.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int lengthOfLIS(int[] nums) {
+        int lis[]=new int[nums.length];
+        Arrays.fill(lis,1);
+        for(int i=1;i<nums.length;i++){
+            for(int j=0;j<i;j++){
+                if(nums[i]>nums[j] && lis[i]<lis[j]+1)
+                    lis[i]=lis[j]+1;
+            }
+        }
+        int max=1;
+        for(int i=0;i<nums.length;i++){
+            max=Math.max(max,lis[i]);
+        }
+        return max;
+    }
+}  
