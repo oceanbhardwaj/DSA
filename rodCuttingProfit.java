a29564b805d7963f3723160d733bdcdaa68a1836
Ocean Bhardwaj <63634357+oceanbhardwaj@users.noreply.github.com>
2022-07-25 08:18:15 +0530

    Add files via upload

---
+++ rodCuttingProfit.java
@@ -0,0 +1,15 @@
+ return (int)helper(n,cuts,0);
+    }
+    public long helper(int amount,int cuts[],int index){
+        if(index>=cuts.length && amount>0)
+            return Integer.MAX_VALUE;
+        if(amount==0)
+            return 0;
+      
+            
+        if(cuts[index]<=amount)
+            return Math.min(amount+helper(amount-cuts[index],cuts,index+1),helper(amount,cuts,index+1));
+        else
+            return helper(amount,cuts,index+1);
+    }
+	
