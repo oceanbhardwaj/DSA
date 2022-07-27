72732cbffed0db20553bffe2222da15378fb7cfc
Ocean Bhardwaj <63634357+oceanbhardwaj@users.noreply.github.com>
2022-07-28 01:12:54 +0530

    Add files via upload

--- countIsland.java
+++ countIsland.java
@@ -1,127 +1,49 @@
 class Solution {
-    class Pair
-    {
+    class Pair{
         int x;
         int y;
-        Pair(int x,int y)
-        {
+        public Pair(int x,int y){
             this.x=x;
             this.y=y;
         }
     }
     public int numIslands(char[][] grid) {
-        
+      
         int n=grid.length;
         int m=grid[0].length;
         int count=0;
         boolean visited[][]=new boolean[n][m];
-        for(int i=0;i<n;i++)
-        {
-            for(int j=0;j<m;j++)
-            {
+        for(int i=0;i<grid.length;i++){
+            for(int j=0;j<grid[0].length;j++){
                 if(!visited[i][j] && grid[i][j]=='1')
-                {  bfs(grid,visited,i,j,n,m);
-                count++;}
+                {count++;
+                 bfs(i,j,grid,n,m,visited);
+                 
+                }
             }
         }
         return count;
     }
-    
-    public void bfs(char grid[][],boolean visited[][],int x_cor,int y_cor,int n,int m)
-    {
-        int arrX[]={-1,0,1,0};
-        int arrY[]={0,1,0,-1};
-        Queue<Pair> q=new LinkedList<>();
-        q.add(new Pair(x_cor,y_cor));
-        visited[x_cor][y_cor]=true;
-        
-        while(!q.isEmpty())
-        {
+    public void bfs(int i,int j,char grid[][],int n,int m,boolean visited[][]){
+          Queue<Pair>q=new LinkedList<>();
+        q.add(new Pair(i,j));
+        visited[i][j]=true;
+        while(!q.isEmpty()){
             Pair p=q.poll();
-            int X=p.x;
-            int Y=p.y;
-            for(int i=0;i<4;i++)
-            {
-                int X_cor=X+arrX[i];
-                int Y_cor=Y+arrY[i];
-                if(X_cor>=0 && X_cor<n && Y_cor>=0 && Y_cor<m && !visited[X_cor][Y_cor]  && grid[X_cor][Y_cor]=='1')
-                {
-                    visited[X_cor][Y_cor]=true;
-                    q.add(new Pair(X_cor,Y_cor));
-                }
-            }
+            int x=p.x;
+            int y=p.y;
+            int a[]={1,0,-1,0};
+            int b[]={0,1,0,-1};
+           for(int i1=0;i1<4;i1++){
+               int X=a[i1]+x;
+               int Y=b[i1]+y;
+               if(X>=0 && X<n && Y>=0 && Y<m && grid[X][Y]=='1' && !visited[X][Y]){
+                   q.add(new Pair(X,Y));
+                   visited[X][Y]=true;
+               }
+           }
+           
+            
         }
     }
-    
-    
-    
-    
-    
-    
-    
-    
-    
-}
-///////////////////////////////////
-class Solution {
-    class Pair
-    {
-        int x;
-        int y;
-        Pair(int x,int y)
-        {
-            this.x=x;
-            this.y=y;
-        }
-    }
-    public int numIslands(char[][] grid) {
-        
-        int n=grid.length;
-        int m=grid[0].length;
-        int count=0;
-        boolean visited[][]=new boolean[n][m];
-        for(int i=0;i<n;i++)
-        {
-            for(int j=0;j<m;j++)
-            {
-                if(!visited[i][j] && grid[i][j]=='1')
-                {  dfs(grid,visited,i,j,n,m);
-                count++;}
-            }
-        }
-        return count;
-    }
-    
-    public void dfs(char grid[][],boolean visited[][],int x_cor,int y_cor,int n,int m)
-    {
-        int arrX[]={-1,0,1,0};
-        int arrY[]={0,1,0,-1};
-        
-        if(visited[x_cor][y_cor])
-            return;
-        visited[x_cor][y_cor]=true;
-        
-        int X=x_cor;
-        int Y=y_cor;
-            for(int i=0;i<4;i++)
-            {
-                int X_cor=X+arrX[i];
-                int Y_cor=Y+arrY[i];
-                if(X_cor>=0 && X_cor<n && Y_cor>=0 && Y_cor<m && !visited[X_cor][Y_cor]  && grid[X_cor][Y_cor]=='1')
-                {
-                    dfs(grid,visited,X_cor,Y_cor,n,m);
-                    
-                }
-            }
-        
-    }
-    
-    
-    
-    
-    
-    
-    
-    
-    
 }
