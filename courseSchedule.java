147a771545ceff61b11cd3f15c55279efeafc48a
Ocean Bhardwaj <63634357+oceanbhardwaj@users.noreply.github.com>
2022-07-30 20:36:56 +0530

    Add files via upload

---
+++ courseSchedule.java
@@ -0,0 +1,32 @@
+class Solution {
+    public boolean canFinish(int numCourses, int[][] prerequisites) {
+        ArrayList<ArrayList<Integer>> adj=new ArrayList<>();
+        for(int i=0;i<numCourses;i++){
+            adj.add(new ArrayList<Integer>());
+        }
+        int indegree[]=new int[numCourses];
+        for(int a[]:prerequisites){
+            int start=a[0];
+            int end=a[1];
+            adj.get(end).add(start);
+            indegree[start]++;
+        }
+        Queue<Integer> q=new LinkedList<>();
+        for(int i=0;i<indegree.length;i++){
+            if(indegree[i]==0)
+            q.add(i);
+        }
+        int count=0;
+        while(!q.isEmpty()){
+           int ele=q.poll();
+            count++;
+            for(int i=0;i<adj.get(ele).size();i++){
+                int adjele=adj.get(ele).get(i);
+                indegree[adjele]--;
+                if(indegree[adjele]==0)
+                    q.add(adjele);
+            }
+        }
+        return count==numCourses?true:false;
+    }
+}
