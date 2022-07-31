2b69c64a91458157e152f2bc42601ba8acd45d58
Ocean Bhardwaj <63634357+oceanbhardwaj@users.noreply.github.com>
2022-07-31 07:55:32 +0530

    Add files via upload

---
+++ reearrangeArraymaxAndMin.java
@@ -0,0 +1,163 @@
+TC-O(N)  SC-O(N)
+
+//{ Driver Code Starts
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Main {
+	public static void main (String[] args)throws IOException
+	{
+
+// 		Scanner in = new Scanner(System.in);
+        
+// 		int t = in.nextInt();
+        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+        
+         //testcases
+        int t = Integer.parseInt(read.readLine());
+		
+		while(t-- >0)
+		{
+		    //size of array
+		    int n = Integer.parseInt(read.readLine());
+		    long[] arr= new long[n];
+		    
+		    String str[] = read.readLine().trim().split(" ");
+		    
+		    //adding elements to the array
+		    for(int i=0;i<n;i++)
+		        arr[i] = Long.parseLong(str[i]);
+		    
+		    // StringBuffer sb = new StringBuffer();
+		    
+		    Solution ob = new Solution();
+		    
+		    //calling rearrange() function
+		    ob.rearrange(arr, n);
+		    StringBuffer sb = new StringBuffer();
+		    
+		    //appending and printing the elements
+		    for(int i =0; i < n; i++)
+		     sb.append(arr[i] + " ");
+		    System.out.println(sb);
+		}
+	}
+}
+
+
+
+
+// } Driver Code Ends
+
+
+class Solution{
+    
+    // temp: input array
+    // n: size of array
+    //Function to rearrange  the array elements alternately.
+    public static void rearrange(long arr[], int n){
+        
+      // Arrays.sort(arr);
+       long ans[]=new long[n];
+       int low=0;
+       int high=n-1;
+       boolean flag=true;
+       int count=0;
+      for(int i=0;i<n;i++){
+          if(flag){
+              ans[count++]=arr[high--];
+          }else
+          ans[count++]=arr[low++];
+          
+          flag=!flag;
+      }
+      for(int i=0;i<n;i++){
+          arr[i]=ans[i];
+      }
+        
+    }
+    
+}
+---------------------------------------------------------------------------------
+
+//{ Driver Code Starts
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Main {
+	public static void main (String[] args)throws IOException
+	{
+
+// 		Scanner in = new Scanner(System.in);
+        
+// 		int t = in.nextInt();
+        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+        
+         //testcases
+        int t = Integer.parseInt(read.readLine());
+		
+		while(t-- >0)
+		{
+		    //size of array
+		    int n = Integer.parseInt(read.readLine());
+		    long[] arr= new long[n];
+		    
+		    String str[] = read.readLine().trim().split(" ");
+		    
+		    //adding elements to the array
+		    for(int i=0;i<n;i++)
+		        arr[i] = Long.parseLong(str[i]);
+		    
+		    // StringBuffer sb = new StringBuffer();
+		    
+		    Solution ob = new Solution();
+		    
+		    //calling rearrange() function
+		    ob.rearrange(arr, n);
+		    StringBuffer sb = new StringBuffer();
+		    
+		    //appending and printing the elements
+		    for(int i =0; i < n; i++)
+		     sb.append(arr[i] + " ");
+		    System.out.println(sb);
+		}
+	}
+}
+
+
+
+
+// } Driver Code Ends
+
+
+class Solution{
+    
+    // temp: input array
+    // n: size of array
+    //Function to rearrange  the array elements alternately.
+    public static void rearrange(long arr[], int n){
+        
+      int mini=0;
+      int maxi=n-1;
+      long me=arr[n-1]+1;
+      for(int i=0;i<n;i++){
+          if(i%2==0){//even
+              arr[i]=arr[i]+arr[maxi]%me*me;
+              maxi--;
+          }else{
+              arr[i]=arr[i]+arr[mini]%me*me;
+              mini++;
+          }
+      }
+      for(int i=0;i<n;i++){
+          arr[i]=arr[i]/me;
+      }
+        
+    }
+    
+}
+
+
+
