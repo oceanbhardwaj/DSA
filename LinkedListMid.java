8810b19329b5802697da92a4945a6c582f5e899d
Ocean Bhardwaj <63634357+oceanbhardwaj@users.noreply.github.com>
2022-08-13 12:58:05 +0530

    Add files via upload

---
+++ LinkedListMid.java
@@ -0,0 +1,53 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode middleNode(ListNode head) {
+        //count all
+        if(head==null)
+            return head;
+        int count=0;
+        ListNode n=head;
+        while(n!=null){
+            count++;
+            n=n.next;
+        }
+        for(int i=0;i<count/2;i++){
+            head=head.next;
+            //head=head.next;
+        }
+        return head;
+    }
+}
+
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode middleNode(ListNode head) {
+        if(head==null)
+            return null;
+        ListNode slow=head;
+        ListNode fast=head;
+        while(fast!=null && fast.next!=null){
+            slow=slow.next;
+            fast=fast.next.next;
+        }
+        return slow;
+    }
+}
