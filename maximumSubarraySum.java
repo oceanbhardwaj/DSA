276810d7f49bac8ed245270ad467fe6bf86fbaea
Ocean Bhardwaj <63634357+oceanbhardwaj@users.noreply.github.com>
2022-10-05 09:30:49 +0530

    Add files via upload

--- maximumSubarraySum.java
+++ maximumSubarraySum.java
@@ -1,26 +1,25 @@
 class Solution {
     public int maxSubArray(int[] nums) {
-        int sum=Integer.MIN_VALUE;
+        int sum=0;
+        int max=Integer.MIN_VALUE;
         for(int i=0;i<nums.length;i++){
-            int count=nums[i];
-            sum=Math.max(sum,count);
+            sum=nums[i];
+            max=Math.max(max,sum);
             for(int j=i+1;j<nums.length;j++){
-                count+=nums[j];
-                   sum=Math.max(sum,count);
+                sum+=nums[j];
+                max=Math.max(max,sum);
             }
         }
-        return sum;
+        return max;
     }
 }
-
---------------------------------------------
 class Solution {
     public int maxSubArray(int[] nums) {
-        int meh=nums[0];
         int msf=nums[0];
+        int meh=nums[0];
         for(int i=1;i<nums.length;i++){
-            meh+=nums[i];
-            if(nums[i]>meh)
+            meh=meh+nums[i];
+            if(meh<nums[i])
                 meh=nums[i];
             msf=Math.max(msf,meh);
         }
--- pow.java
+++ pow.java
@@ -1,59 +1,28 @@
 class Solution {
-    public double myPow(double x, int n) {
-       
-        
-        boolean isNeg=false;
-         long nn=(long)n;
-        if(nn<0)
-        {
-            nn=nn*-1;
-           
-            isNeg=true;
+    public int maxSubArray(int[] nums) {
+        int sum=0;
+        int max=Integer.MIN_VALUE;
+        for(int i=0;i<nums.length;i++){
+            sum=nums[i];
+            max=Math.max(max,sum);
+            for(int j=i+1;j<nums.length;j++){
+                sum+=nums[j];
+                max=Math.max(max,sum);
+            }
         }
-       
-        double ans=1.0;
-        while(nn>0)
-        {
-        if(nn%2==0)
-        {
-            x=x*x;
-            nn=nn/2;
-        }
-        else
-        {
-            ans=ans*x;
-            nn=nn-1;
-        }
-        }
-        System.out.println("ans"+ans);
-        if(isNeg)
-            ans=1/ans;
-        return ans;
-        
-        
+        return max;
     }
 }
-//////////////////////////////////////////////////
 class Solution {
-    public double myPow(double x, int n) {
-        if(n<0)
-        {
-            x=1/x;
-            n=-1*n;
+    public int maxSubArray(int[] nums) {
+        int msf=nums[0];
+        int meh=nums[0];
+        for(int i=1;i<nums.length;i++){
+            meh=meh+nums[i];
+            if(meh<nums[i])
+                meh=nums[i];
+            msf=Math.max(msf,meh);
         }
-        return pow(x,n);
-        
-    }
-    public double pow(double x,int n)
-    {
-        if(n==0)
-            return 1;
-        if(n==1)
-            return x;
-        if(n%2==0)
-        {
-            return pow(x*x,n/2);
-        }
-        return x*pow(x*x,n/2);
+        return msf;
     }
 }
