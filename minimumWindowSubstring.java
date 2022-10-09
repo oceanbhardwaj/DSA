b59571370644d01001aa3986e71c260a2f8a2578
Ocean Bhardwaj <63634357+oceanbhardwaj@users.noreply.github.com>
2022-10-09 17:54:39 +0530

    Add files via upload

--- minimumWindowSubstring.java
+++ minimumWindowSubstring.java
@@ -1,55 +1,52 @@
 class Solution {
     public String minWindow(String s, String t) {
-    HashMap<Character,Integer> pat=new HashMap<>();
-     HashMap<Character,Integer> map=new HashMap<>();
-        if(s.length()<t.length())
-            return "";
-        //hashmap of pattern
+        HashMap<Character,Integer> map2=new HashMap<>();
         for(int i=0;i<t.length();i++){
-            pat.put(t.charAt(i),pat.getOrDefault(t.charAt(i),0)+1);
+            map2.put(t.charAt(i),map2.getOrDefault(t.charAt(i),0)+1);
         }
-        int i=0;
-        int j=0;
+        String ans="";
+        String res="";
+        int max=Integer.MAX_VALUE;
+        int j=-1;
+        int i=-1;
+        int totalLength=t.length();
+        
+        HashMap<Character,Integer>map1=new HashMap<>();
+        //accquire
+        int count=0;
         boolean flag1=false;
         boolean flag2=false;
-        String ans="";
-        int patLength=t.length();
-        int matchCount=0;
         while(true){
-         //acquire
             flag1=false;
-            flag2=false;
-         while(i<s.length() && matchCount!=patLength) {
-             flag1=true;
-              char ch=s.charAt(i);
-           map.put(ch,map.getOrDefault(ch,0)+1);
-             //update match Count
-             if(map.getOrDefault(ch,0)<=pat.getOrDefault(ch,0))
-               matchCount++;  
-             i++;
-           }  
-           //release
-            while(j<s.length() && matchCount==patLength){
-                 flag2=true;
-                String str=s.substring(j,i);
-                if(ans.length()==0 || ans.length()>str.length()){
-                    ans=str;
-                }
-               
-                //update map1
-             char c=s.charAt(j);
-            map.put(c,map.getOrDefault(c,0)-1);
-                
-                //update match count
-               if(map.getOrDefault(c,0)<pat.getOrDefault(c,0))
-                   matchCount--;
-                
-                j++;
-            }
-        if(!flag1 && !flag2)
-            break;
-        
+         flag2=false;
+        while(i<s.length()-1 && count<totalLength){
+            i++;
+            map1.put(s.charAt(i),map1.getOrDefault(s.charAt(i),0)+1);
+            if(map1.getOrDefault(s.charAt(i),0)<=map2.getOrDefault(s.charAt(i),0))
+                count++;
+            flag1=true;
         }
-        return ans;
+        
+        //collect
+        while(j<i && count==totalLength){
+            ans=s.substring(j+1,i+1);
+            if(ans.length()<max)
+            {
+                max=ans.length();
+                res=ans;
+            }
+            j++;
+            if(map1.get(s.charAt(j))==1)
+                map1.remove(s.charAt(j));
+            else
+            map1.put(s.charAt(j),map1.get(s.charAt(j))-1);
+            if(map1.getOrDefault(s.charAt(j),0)<map2.getOrDefault(s.charAt(j),0))
+                count--;
+            flag2=true;
+        }
+            if(!flag1 && !flag2)
+                break;
+        }
+        return res;
     }
 }
